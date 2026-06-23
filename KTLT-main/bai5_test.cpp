be192c24222b9629615065de71705b8721db7795
NgocMihMicamica <ngocminhhust7711@gmail.com>
2026-06-23 23:15:44 +0700

    Create bai5_test.cpp

---
+++ KTLT-main/bai5_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>
+
+// 1. Include header của các file cần test từ thư mục include
+// #include "../include/question.h"
+// #include "../include/exam.h"
+
+void test_ten_chuc_nang() {
+    // 2. Viết test case
+    // int result = ham_can_test();
+    // assert(result == ket_qua_mong_doi); 
+}
+
+int main() {
+    // 3. Gọi các hàm test
+    test_ten_chuc_nang();
+    
+    std::cout << "Pass tat ca test case!\n";
+    return 0;
+}
